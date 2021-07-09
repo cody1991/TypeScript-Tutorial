80d56ec0f652c0b3348494af57975cd95be124d7
codytang <codytang@tencent.com>
2021-07-09 11:52:46 +0800

    build: 更新 commit

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m '🎉🎉 又发布新内容啦 🎉🎉'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
